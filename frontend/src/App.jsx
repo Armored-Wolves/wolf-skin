f229e009bd3aa82ffd2c16e75e59b040abf09f2c
VitaliiSeptember <vitaliiseptember@gmail.com>
2024-05-14 19:22:05 +0300

    fixed routed to Home page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -31,6 +31,7 @@ function App() {
         />
         <Route path="/login" element={<Login />} />
         <Route path="/registration" element={<Registration />} />
+        <Route path="/home" element={<Home />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -8,20 +8,6 @@ function Home() {
     const [content, setContent] = useState("");
     const [title, setTitle] = useState("");
 
-    useEffect(() => {
-        getNotes();
-    }, []);
-
-    const getNotes = () => {
-        api
-            .get("/api/notes/")
-            .then((res) => res.data)
-            .then((data) => {
-                setNotes(data);
-                console.log(data);
-            })
-            .catch((err) => alert(err));
-    };
 
     const deleteNote = (id) => {
         api
