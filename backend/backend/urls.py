763e582f20b792f59c62e3d7d79bbec1a5fbe6cc
VitaliiSeptember <vitaliiseptember@gmail.com>
2024-05-14 19:41:44 +0300

    added routes for Notes(todo)

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('api.urls'))  # routing configuration
+    path('api/', include('api.urls')),  # routing configuration
+    path('notes/', include('api.urls'))
 ]
